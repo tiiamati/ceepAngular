f9e370bbe3d403ea2e69f1209d25df72907ec4ec
Leticia Corral <le.martincorral@gmail.com>
2018-01-30 11:43:56 -0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MuralComponent } from './mural/mural.component';
+import { FormNovoCartaoComponent } from './form-novo-cartao/form-novo-cartao.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MuralComponent,
+    FormNovoCartaoComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule
